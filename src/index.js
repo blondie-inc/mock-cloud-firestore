905a64c1ec258b8ff490863b8de5bf66d08fe091
jasnoponjatno <example@example.com>
2019-04-10 12:01:18 +0300

    exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import MockFirebase from './firebase';
+import CollectionReference from './firebase/firestore/collection-reference';
+import Query from './firebase/firestore/query';
+import DocumentReference from './firebase/firestore/document-reference';
 
 export default MockFirebase;
+export { CollectionReference, Query, DocumentReference };
